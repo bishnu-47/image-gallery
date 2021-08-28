7449f5cf58e30c43b30d88bf448cce5208f470a7
bishnu-47 <bd23122000@gmail.com>
2021-08-28 09:42:41 +0530

    added pagination

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from "react";
+import { Pagination } from "antd";
 
 import ImageCard from "./components/image-card.js";
 import SearchForm from "./components/search-form.js";
 
 function App() {
   const [images, setImages] = useState([]);
+  const [pageNum, setPageNum] = useState(1);
+  const [itemsPerPage, setItemsPerPage] = useState(20);
   const [isLoading, setIsLoading] = useState(true);
   const [searchQuery, setSearchQuery] = useState("");
 
@@ -13,7 +16,7 @@ function App() {
       try {
         setIsLoading(true);
         const res = await fetch(
-          `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchQuery}&image_type=photo`
+          `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchQuery}&image_type=photo&page=${pageNum}&per_page=${itemsPerPage}&safesearch=true`
         );
         const data = await res.json();
 
@@ -25,7 +28,7 @@ function App() {
     };
 
     fetchData();
-  }, [searchQuery]);
+  }, [searchQuery, pageNum, itemsPerPage]);
 
   function search(term) {
     const t = term.trim();
@@ -43,11 +46,23 @@ function App() {
       {isLoading ? (
         <p className="text-3xl font-semibold text-gray-800">Loading...</p>
       ) : (
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-          {images.map((image) => (
-            <ImageCard key={image.id} image={image} />
-          ))}
-        </div>
+        <>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+            {images.map((image) => (
+              <ImageCard key={image.id} image={image} />
+            ))}
+          </div>
+
+          <div className="w-full mx-auto mt-8 text-center">
+            <Pagination
+              current={pageNum}
+              pageSize={itemsPerPage}
+              total={200}
+              onChange={(page, pageSize) => setPageNum(page)}
+              onShowSizeChange={(current, size) => setItemsPerPage(size)}
+            />
+          </div>
+        </>
       )}
     </div>
   );
